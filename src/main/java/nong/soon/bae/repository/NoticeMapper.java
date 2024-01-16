96feb9b83e2b6a5c2047b98c4be2418719a8cf2a
nini0912 <lovetloob1@naver.com>
2024-01-16 17:34:29 +0900

    관리자 공지게시판 만들기

---
+++ src/main/java/nong/soon/bae/repository/NoticeMapper.java
@@ -0,0 +1,19 @@
+package nong.soon.bae.repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+import nong.soon.bae.bean.NoticeBoardDTO;
+
+public interface NoticeMapper {
+
+	public void insert (NoticeBoardDTO dto); //작성글 인서트
+	public int count(); //작성글 총 갯수 
+	public List<NoticeBoardDTO> list (HashMap map); //작성글 목록
+	public void updateCountUp(int num); //조회수 증가하기 
+	public NoticeBoardDTO readContent(int num);
+	public int maxNum(); //가장 최신글 번호  
+
+	public int delete(int num); //글 삭제하기
+
+}
