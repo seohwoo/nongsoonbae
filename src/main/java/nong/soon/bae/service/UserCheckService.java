d854c8f920c6e6bec5fe5c78b7908e80d138c89a
nini0912 <lovetloob1@naver.com>
2024-01-18 12:00:21 +0900

    회원관리 하는 중

--- src/main/java/nong/soon/bae/service/UserCheckService.java
+++ src/main/java/nong/soon/bae/service/UserCheckService.java
@@ -4,5 +4,5 @@ import org.springframework.ui.Model;
 
 public interface UserCheckService {
 	public void userlist(int pageNum , Model model); //일반 유저 목록
-	
+	public String userstop (String username, String reason); //유저 정지하기 
 }
